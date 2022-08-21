70d1bb29c72dd0f6233cb425ce032cb0ea9537d0
unafoca <chuqiaojo.wang@gmail.com>
2022-08-20 21:33:58 -0400

    Finish Step 3

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    actionset = Array('Rock', 'Paper', 'Scissor')
+    let x = Math.floor(Math.random() * 3)
+    console.log(x)
+    return actionset[x]
+  }
+
+console.log(getComputerChoice())
