460ea30622dcb555f8f2103277b07c82f208a3f8
unafoca <chuqiaojo.wang@gmail.com>
2022-09-08 21:44:06 -0400

    finish question 4

--- javascript.js
+++ javascript.js
@@ -3,8 +3,22 @@ console.log("Hello World")
 function getComputerChoice() {
     actionset = Array('Rock', 'Paper', 'Scissor')
     let x = Math.floor(Math.random() * 3)
-    console.log(x)
+    // console.log(x)
     return actionset[x]
   }
 
-console.log(getComputerChoice())
+  function singleRound(playerSelection, computerSelection) {
+    actionset = Array('Rock', 'Paper', 'Scissor')
+    playerIndex = actionset.findIndex(x => x.toLowerCase() == playerSelection.toLowerCase())
+    computerIndex = actionset.findIndex(x => x.toLowerCase() == computerSelection.toLowerCase())
+
+    if (computerIndex - playerIndex == 1 || (computerIndex == 0 && playerIndex == 2)) {
+        return "You Lose!"
+    } else if (computerIndex == playerIndex) {
+        return "Tie!"
+    } else {
+        return "You Win!"
+    }
+  }
+
+// console.log(getComputerChoice())
